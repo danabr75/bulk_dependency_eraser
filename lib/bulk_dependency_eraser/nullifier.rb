b322c2378b503b392bcbe7a21c42b244a96a7a77
Benjamin Dana <benrdana@gmail.com>
2024-09-02 18:29:47 -0400

    Updating Nullifier. Now grouping columns into single SQL calls, when the IDs are the same. Also adding safety-net of 'disable_referential_integrity' disable option.

--- lib/bulk_dependency_eraser/nullifier.rb
+++ lib/bulk_dependency_eraser/nullifier.rb
@@ -2,7 +2,11 @@ module BulkDependencyEraser
   class Nullifier < Base
     DEFAULT_OPTS = {
       verbose: false,
-      db_nullify_wrapper: self::DEFAULT_DB_WRAPPER
+      db_nullify_wrapper: self::DEFAULT_DB_WRAPPER,
+      # Set to true if you want 'ActiveRecord::InvalidForeignKey' errors raised during nullifications
+      # - I can't think of a use-case where a nullification would generate an invalid key error
+      # - Not hurting anything to leave it in, but might remove it in the future.
+      enable_invalid_foreign_key_detection: false
     }.freeze
 
     DEFAULT_DB_WRAPPER = ->(block) do
@@ -22,6 +26,53 @@ module BulkDependencyEraser
     def initialize class_names_columns_and_ids:, opts: {}
       @class_names_columns_and_ids = class_names_columns_and_ids
       super(opts:)
+
+      if opts_c.verbose
+        puts "Combining nullification column groups (if groupable)"
+        puts "Before Combination: #{@class_names_columns_and_ids}"
+      end
+
+      @class_names_columns_and_ids = combine_matching_columns(@class_names_columns_and_ids)
+
+      if opts_c.verbose
+        puts "After Combination: #{@class_names_columns_and_ids}"
+      end
+    end
+
+    # Combine columns if the IDs are the same
+    # - will do one SQL call instead of several
+    def combine_matching_columns(nullification_hash)
+      return {} if nullification_hash.none?
+
+      merged_hash = {}
+
+      nullification_hash.each do |klass_name, columns_and_ids|
+        merged_hash[klass_name] = {}
+        columns_and_ids.each do |key, array|
+          sorted_array = array.sort
+
+          # Find any existing key in merged_hash that has the same sorted array
+          matching_key = merged_hash[klass_name].keys.find { |k| merged_hash[klass_name][k].sort == sorted_array }
+
+          if matching_key
+            # Concatenate the matching keys and update the hash
+            new_key = key.is_a?(Array) ? key : [key]
+            if matching_key.is_a?(Array)
+              new_key += matching_key
+            else
+              new_key << matching_key
+            end
+
+            merged_hash[klass_name][new_key] = sorted_array
+            merged_hash[klass_name].delete(matching_key)
+          else
+            # Otherwise, just add the current key-value pair
+            merged_hash[klass_name][key] = sorted_array
+          end
+        end
+      end
+
+      merged_hash
     end
 
     def execute
@@ -34,15 +85,19 @@ module BulkDependencyEraser
             klass = class_name.constantize
 
             columns_and_ids = class_names_columns_and_ids[class_name]
-
             columns_and_ids.each do |column, ids|
               current_column = column
-              # Disable any ActiveRecord::InvalidForeignKey raised errors.
-              # src https://stackoverflow.com/questions/41005849/rails-migrations-temporarily-ignore-foreign-key-constraint
-              #     https://apidock.com/rails/ActiveRecord/ConnectionAdapters/AbstractAdapter/disable_referential_integrity
-              ActiveRecord::Base.connection.disable_referential_integrity do
-                nullify_in_db do
-                  klass.unscoped.where(id: ids).update_all(column => nil)
+
+              if opts_c.enable_invalid_foreign_key_detection
+                # nullify with referential integrity
+                nullify_by_klass_column_and_ids(klass, column, ids)
+              else
+                # nullify without referential integrity
+                # Disable any ActiveRecord::InvalidForeignKey raised errors.
+                # - src: https://stackoverflow.com/questions/41005849/rails-migrations-temporarily-ignore-foreign-key-constraint
+                #        https://apidock.com/rails/ActiveRecord/ConnectionAdapters/AbstractAdapter/disable_referential_integrity
+                ActiveRecord::Base.connection.disable_referential_integrity do
+                  nullify_by_klass_column_and_ids(klass, column, ids)
                 end
               end
             end
@@ -59,6 +114,23 @@ module BulkDependencyEraser
     protected
 
     attr_reader :class_names_columns_and_ids
+
+    def nullify_by_klass_column_and_ids klass, columns, ids
+      nullify_columns = {}
+
+      # supporting nullification of groups of columns simultaneously
+      if columns.is_a?(Array)
+        columns.each do |column|
+          nullify_columns[column] = nil
+        end
+      else
+        nullify_columns[columns] = nil
+      end
+
+      nullify_in_db do
+        klass.unscoped.where(id: ids).update_all(nullify_columns)
+      end
+    end
 
     def nullify_in_db(&block)
       puts "Nullifying from DB..." if opts_c.verbose
